d8521be03ac21b262985e1c2408bacaf9a62f879
sergiolizanamontero <slizana92@gmail.com>
2017-06-16 09:34:01 +0200

    Added WidgetService

--- app/src/main/java/riviasoftware/recipesapp/widget/WidgetService.java
+++ app/src/main/java/riviasoftware/recipesapp/widget/WidgetService.java
@@ -1,7 +1,9 @@
 package riviasoftware.recipesapp.widget;
 
 import android.content.Intent;
+import android.util.Log;
 import android.widget.RemoteViewsService;
+import android.widget.Toast;
 
 /**
  * Created by sergiolizanamontero on 6/6/17.
@@ -11,6 +13,5 @@ public class WidgetService extends RemoteViewsService {
 
     public RemoteViewsService.RemoteViewsFactory onGetViewFactory(Intent intent) {
         return new ListProvider(this.getApplicationContext());
-
     }
 }
